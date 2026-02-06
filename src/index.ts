1e8cf863be33116136aec8fd0950cd2558c617f6
a.dacapo21 <aggeloskappos@gmail.com>
2026-02-06 19:29:48 +0200

    Export multi-exchange connectors and exchange factory

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,12 @@ export { BaseStrategy } from './core/strategy/base-strategy';
 
 // Exchange connectors
 export { MexcConnector } from './exchanges/mexc/mexc-connector';
+export { GateioConnector } from './exchanges/gateio/gateio-connector';
+export { BitgetConnector } from './exchanges/bitget/bitget-connector';
+export  { KrakenConnector } from './exchanges/kraken/kraken-connector';
 
+// Exchange factory
+export { ExchangeFactory, SupportedExchange } from './cli/exchange-factory';
 import { createLogger } from './utils';
 
 export class OpenMM {
